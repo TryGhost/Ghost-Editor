ce750cda1925bbcdcee449826303e1d6677aec90
Estevao Soares dos Santos <estevao.santos@gmail.com>
2017-01-29 06:49:07 +0000

    Make Ghost Editor compatible with showdown latest
     - changed showdown global
     - turned a bunch of options on in showdown to set a "ghost" flavor markdown, similar to GFM
     - custom extensions live in their own repo, with proper tests against showdown
     - updated dependencies in package.json

--- addon/cards/markdown-card_html.js
+++ addon/cards/markdown-card_html.js
@@ -2,8 +2,28 @@
 // this has to be a node style module as it's imported by the front end and ember.
 // a card has to have an editor object, and an HTML object. In the future we might have AMP and Text (for FTS) renderers.
 
-var Showdown        = require('showdown-ghost'),
-    converter        = new Showdown.converter({extensions: ['ghostgfm', 'footnotes', 'highlight']});
+var showdown        = require('showdown'),
+    sdGhostExtra    = require('showdown-ghost-extra'),
+    footnotes       = require('showdown-ghost-footnotes'),
+    highlight       = require('showdown-ghost-highlight'),
+    converter        = new showdown.Converter({
+        extensions: [sdGhostExtra, footnotes, highlight],
+        omitExtraWLInCodeBlocks: true,
+        parseImgDimensions: true,
+        simplifiedAutoLink: true,
+        excludeTrailingPunctuationFromURLs: true,
+        literalMidWordUnderscores: true,
+        strikethrough: true,
+        tables: true,
+        tablesHeaderId: true,
+        ghCodeBlocks: true,
+        tasklists: true,
+        smoothLivePreview: true,
+        simpleLineBreaks: true,
+        requireSpaceBeforeHeadingText: true,
+        ghMentions: false,
+        encodeEmails: true
+    });
 
 
 module.exports = {
